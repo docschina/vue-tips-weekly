5f02cd1cd266d96458e7016f79e95da731389955
Otto <fa@ijust.cc>
2022-07-14 00:12:57 +0800

    feat: 初始化工作流

---
+++ app.js
@@ -0,0 +1,67 @@
+const html2md = require("html-to-md");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs/promises");
+const path = require("path");
+const cli = require("cac")();
+
+cli.option("--url <url>", "输入网址", {
+  default: "",
+});
+const { args, options } = cli.parse();
+console.log(args);
+
+// const prettier = require("prettier");
+
+const url = args.length === 1 ? args[0] : null;
+if (!url) {
+  console.log("No url specified");
+  return;
+}
+
+const main = () => {
+  axios
+    .get(url)
+    .then((response) => {
+      const rawHTML = response.data;
+      if (!rawHTML) {
+        console.log("No HTML found");
+        return;
+      }
+      const $ = cheerio.load(rawHTML);
+      const title = $(".post-header").text();
+      let index = url.match(/weekly-(\d*)/);
+      console.log(title);
+      index = Array.isArray(index) && index.length > 1 ? index[1] : "";
+
+      let post = $(".post-grid").html();
+      if (!post) {
+        console.log("No post found");
+        return;
+      }
+      post = `# ${title}\n${post}\n## 来源\n\n${url}\n`;
+      const md = html2md(post, {}, false);
+      //  prettier.resolveConfig(filePath).then((options) => {
+      //    const md = prettier.format(md, options);
+      //  });
+
+      return {
+        title,
+        index,
+        md,
+      };
+    })
+    .then(async ({ index, md }) => {
+      const _path = path.resolve(__dirname, "posts/" + index + ".md");
+
+      // prettier.format(text);
+      return fs.writeFile(_path, md, "utf8");
+    })
+    .then(() => {
+      console.log("done");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+main();
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Vue Tips Weekly
+
+## 来源
+
+- 访问 https://michaelnthiessen.com/
+- 找到 Articles 选择 All
+- 获取 URL
+
+## 工作流
+
+目前试运行。北京时间每周三晚上八点会收到 weekly。
+
+目前使用 `node app xxx.com` 的方式来爬取文章内容，具体可参考 `app.js` 里的内容。
+
+后续考虑做成自动化的。
